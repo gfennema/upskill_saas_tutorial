89d970c4db88abdeb615dd43b879c4152feeab13
Gregory Fennema <gfennema@live.com>
2017-11-20 19:50:20 +0000

    flash messages changed incorrectly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   before_action :configure_premitted_parameters, if: :devise_controller?
+  # Whitelist the form fields so that we can process them, if coming from a devise sign up form
   
   
   protected
