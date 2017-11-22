abaf91401fbfc2f890228bd2a21c22e950fd2e0f
Gregory Fennema <gfennema@live.com>
2017-11-22 17:49:34 +0000

    re-added profile and user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
          
   belongs_to :plan
-  
+  has_one :profile
   
   attr_accessor :stripe_card_token
   # If Pro User passes validations, then call Stripe and tell Stripe
